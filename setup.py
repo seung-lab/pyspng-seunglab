f2c5e2974ba197b2fd6136db93ffb7442630f44d
William Silversmith <william.silversmith@gmail.com>
2022-02-22 19:24:55 -0500

    build: add Trove classifiers

--- setup.py
+++ setup.py
@@ -75,4 +75,19 @@ setup(
             "pyspng=pyspng_cli:main"
         ],
     },
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Topic :: Scientific/Engineering :: Image Processing",
+        "Topic :: System :: Archiving :: Compression",
+    ],
 )
